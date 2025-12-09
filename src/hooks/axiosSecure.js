84d6d9d3de850fd8d901b6ab2b0f37407557669c
Ahmed Numan <reduanahmed773@gmail.com>
2025-12-09 21:10:41 +0600

    AxiosSecure not defined problem fixed

--- src/hooks/axiosSecure.js
+++ src/hooks/axiosSecure.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://fb-downloader-server.vercel.app";
 
+const axiosSecure = axios.create({
+  // You can add custom configuration here if needed
+});
 export default axiosSecure;
